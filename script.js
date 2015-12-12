aa0e244fadb21b4ff709cea129ff3d64b1e3e3e1
Coira <Coira.ells@gmail.com>
2015-12-12 17:11:43 +0000

    player clicks, board display, ready for AI

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {
+    
     $(".square").click(function() {
-	console.log($(this).attr("id"));
+	playerTurn($(this));
     });
+
+    var board = [];
+    var playerSign = "O";
+    var aiSign = "X";
+    var order = [];
+    
+    function start() {
+	for (var i = 0; i < 9; i++) {
+	    board[i] = "";
+	}
+
+	// clear board
+	// ask whether player wants to be X or O
+    }
+
+    function playerTurn(square) {
+
+	var index = parseInt(square.attr("id")[1]);
+
+	if (!board[index]) {
+	    square.text(playerSign);
+	    board[index] = playerSign;
+	    order.push(index);
+	    
+	    //console.log(board);
+	    console.log(order);
+	    aiTurn();
+	}
+    }
+
+    function aiTurn(){
+	for (var i = 0; i < 9; i++) {
+	    if (!board[i]) {
+		board[i] = aiSign;
+		$("#s"+i).text(aiSign);
+		order.push(i);
+		console.log(order);
+		break;
+	    }
+	}
+    }
+
+	
 
 });
 
